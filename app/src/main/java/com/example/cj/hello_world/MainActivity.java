73554b2d001d014f1e76c5e8a414e4f775eecfdf
changburi <changbur@gmail.com>
2017-02-05 23:30:23 +0900

    2017-02-05 added log

--- app/src/main/java/com/example/cj/hello_world/MainActivity.java
+++ app/src/main/java/com/example/cj/hello_world/MainActivity.java
@@ -1,13 +1,15 @@
 package com.example.cj.hello_world;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
-
+  protected final String TAG = MainActivity.class.getSimpleName();
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    Log.i(TAG, "onCreate");
   }
 }
